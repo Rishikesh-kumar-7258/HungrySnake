453fac5a27f6391d01608dcedbf57f1561eb805e
Rishikesh-kumar-7258 <69416888+Rishikesh-kumar-7258@users.noreply.github.com>
2021-05-10 20:41:35 +0530

    Added controls for mobile

--- js/board.js
+++ js/board.js
@@ -1,30 +1,27 @@
 // variables
-let direction = {x : 0, y : 0};
+let direction = { x: 0, y: 0 };
 let speed = 10;
 let lastPaintTime = 0;
 let score = 0;
 let highScore = localStorage.getItem('hiscore');
 
-if (highScore === null)
-{
+if (highScore === null) {
     localStorage.setItem('hiscore', 0);
     highScore = localStorage.getItem('hiscore');
 }
 
 let snakeArr = [
-    {x : 5, y : 10}
+    { x: 5, y: 10 }
 ]
 
-let Food = { x : 9, y : 15};
+let Food = { x: 9, y: 15 };
 
 highScoreBoard.innerHTML = `HighScore : ${highScore}`;
 
 // main function
-function main(ctime)
-{
+function main(ctime) {
     window.requestAnimationFrame(main);
-    if ((ctime - lastPaintTime) / 1000 < 1/speed)
-    {
+    if ((ctime - lastPaintTime) / 1000 < 1 / speed) {
         return;
     }
     lastPaintTime = ctime;
@@ -35,78 +32,68 @@ window.requestAnimationFrame(main);
 window.addEventListener('keydown', e => {
 
 
-    switch(e.key){
+    switch (e.key) {
         case 'ArrowUp':
-            if (direction.y === 0)
-            {
+            if (direction.y === 0) {
                 direction.x = 0;
                 direction.y = -1;
             }
             break;
         case 'ArrowDown':
-            if (direction.y === 0)
-            {
+            if (direction.y === 0) {
                 direction.x = 0;
                 direction.y = 1;
             }
             break;
         case 'ArrowLeft':
-            if (direction.x === 0)
-            {
+            if (direction.x === 0) {
                 direction.x = -1;
                 direction.y = 0;
             }
             break;
         case 'ArrowRight':
-            if (direction.x === 0)
-            {
+            if (direction.x === 0) {
                 direction.x = 1;
                 direction.y = 0;
             }
             break;
-        default :
+        default:
             break;
     }
 })
 
-function isCollision(snakeArr)
-{
+function isCollision(snakeArr) {
     let first = snakeArr[0];
-    if (first.x <= 0 || first.x > 20 || first.y <= 0 || first.y > 20)
-    {
+    if (first.x <= 0 || first.x > 20 || first.y <= 0 || first.y > 20) {
         return true;
     }
 
-    for (let i = 1; i < snakeArr.length; i++)
-    {
+    for (let i = 1; i < snakeArr.length; i++) {
         if (first.x === snakeArr[i].x && first.y === snakeArr[i].y) return true;
     }
 
     return false;
 }
 
-function gameEngine()
-{
+function gameEngine() {
 
     // If collision occurs
-    if (isCollision(snakeArr))
-    {
-        direction = {x : 0, y : 0};
+    if (isCollision(snakeArr)) {
+        direction = { x: 0, y: 0 };
         score = 0;
         localStorage.setItem('hiscore', highScore);
         alert("Game Over! press any key to continue");
         scoreBoard.innerHTML = `Score : ${score}`;
         snakeArr = [
-            {x : 5, y : 10}
+            { x: 5, y: 10 }
         ]
         gameEngine();
     }
 
     // when Snake eates the food
-    if (snakeArr[0].x == Food.x && snakeArr[0].y == Food.y)
-    {
-        snakeArr.unshift({x : snakeArr[0].x + direction.x , y : snakeArr[0].y + direction.y});
-        score += speed/2;
+    if (snakeArr[0].x == Food.x && snakeArr[0].y == Food.y) {
+        snakeArr.unshift({ x: snakeArr[0].x + direction.x, y: snakeArr[0].y + direction.y });
+        score += speed / 2;
         highScore = Math.max(score, highScore);
         scoreBoard.innerHTML = `Score : ${score}`;
         highScoreBoard.innerHTML = `HighScore : ${highScore}`;
@@ -114,7 +101,7 @@ function gameEngine()
         let b = 18;
 
         do {
-            Food = {x : Math.round(a + (b-a)*Math.random()), y : Math.round(a + (b-a)*Math.random())};  
+            Food = { x: Math.round(a + (b - a) * Math.random()), y: Math.round(a + (b - a) * Math.random()) };
         } while (isOccupied(Food));
 
     }
@@ -131,9 +118,8 @@ function gameEngine()
     })
 
     //moving the snake
-    for (let i = snakeArr.length - 2; i >= 0; i--)
-    {
-        snakeArr[i+1] = {...snakeArr[i]};
+    for (let i = snakeArr.length - 2; i >= 0; i--) {
+        snakeArr[i + 1] = { ...snakeArr[i] };
     }
     snakeArr[0].x += direction.x;
     snakeArr[0].y += direction.y;
@@ -147,11 +133,56 @@ function gameEngine()
 }
 
 // checking if the food spawn is not in the snake area
-function isOccupied(Food)
-{
+function isOccupied(Food) {
     snakeArr.forEach(e => {
         if (e.x === Food.x && e.y === Food.y) return 1;
     })
 
     return 0;
 }
+
+// Making controls for mobiles
+tgbtn.addEventListener('click', () => {
+    if (controls.style.display === "grid")
+    {
+        controls.style.display = "none";
+    }
+    else{
+        controls.style.display = "grid";
+    }
+})
+
+let arrow = document.querySelectorAll('.arrow');
+
+arrow.forEach((e, i) => {
+    e.addEventListener('click', () => {
+        // console.log(e.classList[1]);
+
+        switch (e.classList[1]) {
+            case 'ctup':
+                if (direction.y === 0) {
+                    direction.x = 0;
+                    direction.y = -1;
+                }
+                break;
+            case 'ctdown':
+                if (direction.y === 0) {
+                    direction.x = 0;
+                    direction.y = 1;
+                }
+                break;
+            case 'ctleft':
+                if (direction.x === 0) {
+                    direction.x = -1;
+                    direction.y = 0;
+                }
+                break;
+            case 'ctright':
+                if (direction.x === 0) {
+                    direction.x = 1;
+                    direction.y = 0;
+                }
+                break;
+        }
+    })
+})
